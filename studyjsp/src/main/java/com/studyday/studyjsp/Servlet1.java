e9b57c4b66ff04a960cd0ca48574c7ab0f6aafd7
luoqi <luoqi@yinhai.com>
2020-08-31 23:32:35 +0800

    1.pom 在总项目中维护 2.实现使用web.xml 和 注解@WebSerlet方式实现sevlet。 3.列出最近计划

---
+++ studyjsp/src/main/java/com/studyday/studyjsp/Servlet1.java
@@ -0,0 +1,38 @@
+package com.studyday.studyjsp;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 使用注解发布
+ * @author lq
+ *
+ */
+@WebServlet(name="Servlet1",urlPatterns = {"/myservlet1"})
+public class Servlet1 extends HttpServlet {
+	  private static final long serialVersionUID = 1L;
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.service(req, resp);
+		String param1 = req.getParameter("parm1");
+		System.out.println(param1);
+		PrintStream printStream = new PrintStream(resp.getOutputStream());
+		printStream.println("<html>");
+		printStream.println("<h2>");
+		printStream.println("Welcom! this is servlet1,");
+		printStream.println("</h2>");
+		printStream.println("</html>");
+	
+		System.out.println("I am coming");
+	}
+	
+	
+}
---
+++ studyjsp/src/main/java/com/studyday/studyjsp/Servlet2.java
@@ -0,0 +1,54 @@
+package com.studyday.studyjsp;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * web.xml 配置发布的servlet
+ * @author lq
+ *
+ */
+public class Servlet2 extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	 
+		System.out.println("get! welcome");
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintStream printStream = new PrintStream(resp.getOutputStream());
+		printStream.println("<html>");
+		printStream.println("<h2>");
+		printStream.println("Welcom! this is servlet2,");
+		printStream.println("</h2>");
+		printStream.println("</html>");
+		
+	
+	}
+	
+//	@Override
+//	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		//super.doPost(req, resp);
+//		resp.setContentType("text/html;charSet=GBK");
+//		System.out.println("get! welcome");
+//		PrintStream printStream = new PrintStream(resp.getOutputStream());
+//		printStream.println("<html>");
+//		printStream.println("<h2>");
+//		printStream.println("Welcom! this is servlet2,");
+//		printStream.println("</h2>");
+//		printStream.println("</html>");
+//
+//	}
+	
+}
