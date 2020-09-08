0241e4501d90daaec977868e75628dbc45805494
luoqi <luoqi@yinhai.com>
2020-09-08 12:57:11 +0800

    1.添加 jar-with-dependencies 2.添加依赖关系图

--- studymvn1/study-b/src/main/java/com/studyday/study/b/Fruit.java
+++ studymvn1/study-b/src/main/java/com/studyday/study/b/Fruit.java
@@ -15,3 +15,20 @@ public class Fruit {
 
 	}
 }
+
+
+//
+//import com.studyday.study.Cherry;
+//
+///**
+// * Hello world!
+// *
+// */
+//public class Fruit {
+//	public void eatFruit() {
+//		Cherry banana = new Cherry();
+//		String name = banana.getName();//实验1时使用
+//		System.out.println("b" + name);
+//
+//	}
+//}
