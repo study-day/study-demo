1ce1b8f38821cca9112aaa4998f60225fdaa0bd5
luoqi <luoqi@yinhai.com>
2020-09-20 21:34:07 +0800

    1.添加myaqa需求文档 2.测试maven依赖，能不能将A模块的代码打包到B模块中，且B模块只生成一个jar包，这种是不行的，这种操作可能存在潜在的问题，比如依赖传递问题

---
+++ studyfile/src/main/java/com/sutdyday/studyfile/Filetools.java
@@ -0,0 +1,30 @@
+package com.sutdyday.studyfile;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class Filetools {
+
+
+    /**
+                * 查找指定路径下所有的文件
+     * @param fileList
+     * @param parentDir
+     */
+    public static void findAllFiles(List<String> fileList,File parentDir) {
+//      File parentFileDir = new File(parentDir);
+//      String[] ChildFile = parentFileDir.list();
+      if(parentDir.isFile()) {
+    	  if(!parentDir.getName().endsWith("pom.xml")) {
+    		  fileList.add(parentDir.getAbsoluteFile().toString());  
+    	  }
+    	  
+      }else {
+    	  for (int i = 0; i<parentDir.list().length;i++) {
+        	  findAllFiles(fileList,new File(parentDir.getAbsolutePath()+File.separator+parentDir.list()[i]));
+          }  
+      }
+    }
+	
+}
