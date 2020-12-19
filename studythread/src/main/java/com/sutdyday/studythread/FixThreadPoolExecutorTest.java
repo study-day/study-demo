816d22a1ece5cba461a1c7cb4d5d6e5d0294ce35
luoqi <luoqi@yinhai.com>
2020-12-19 22:59:56 +0800

    maven多线程学习

--- studythread/src/main/java/com/sutdyday/studythread/FixThreadPoolExecutorTest.java
+++ studythread/src/main/java/com/sutdyday/studythread/FixThreadPoolExecutorTest.java
@@ -10,77 +10,77 @@ import java.util.concurrent.ThreadPoolExecutor;
  * FixThreadPoolExecutorTest 定义一个可重用的固线程数的线程池
  */
 public class FixThreadPoolExecutorTest {
-	public static void main(String[] args) {
-		ExecutorService pool = Executors.newFixedThreadPool(3);
-		Thread th = null;
-		// 始终只有10个线程来处理请求任务，来不及处理的任务会放置于阻塞队列中
-		for (int i = 0; i < 3; i++) {
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        Thread th = null;
+        // 始终只有10个线程来处理请求任务，来不及处理的任务会放置于阻塞队列中
+        for (int i = 0; i < 3; i++) {
 
-			th = new Thread() {
+            th = new Thread() {
 
-				int d = new Random().nextInt() % 100;
+                int d = new Random().nextInt() % 100;
 
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					while (true) {
-						try {
-							Thread.sleep(2000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    while (true) {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
 
-						long x = 1 / (System.currentTimeMillis() % d);
-						System.out.println(Thread.currentThread().getName() + "--" + Thread.activeCount());
+                        long x = 1 / (System.currentTimeMillis() % d);
+                        System.out.println(Thread.currentThread().getName() + "--" + Thread.activeCount());
 
-					}
-				}
-			};
-			th.setName("app"+i);
-			pool.execute(th);
+                    }
+                }
+            };
+            th.setName("app" + i);
+            pool.execute(th);
 
-		}
+        }
 
-		Thread  thread = new Thread() {
+        Thread thread = new Thread() {
 
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				while (true) {
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					long x = 1 / (System.currentTimeMillis() % 4);
-					System.out.println(Thread.currentThread().getName() + "--" + Thread.activeCount()+"@@");
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (true) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    long x = 1 / (System.currentTimeMillis() % 4);
+                    System.out.println(Thread.currentThread().getName() + "--" + Thread.activeCount() + "@@");
 
-				}
-			}
-		};
-		thread.setName("thread123");
-		pool.execute(thread);
-		//pool.shutdown();
+                }
+            }
+        };
+        thread.setName("thread123");
+        pool.execute(thread);
+        //pool.shutdown();
 
-		List<Runnable> shutdownNow = pool.shutdownNow();
-		int i  =0 ;
-        while(true){
-        	
-        	System.out.println("第" +i+++ "次检查");
-           if(pool.isTerminated()){  
+        List<Runnable> shutdownNow = pool.shutdownNow();
+        int i = 0;
+        while (true) {
+
+            System.out.println("第" + i++ + "次检查");
+            if (pool.isTerminated()) {
                 System.out.println("---END---\n");
-                shutdownNow.forEach(threadNotRun->System.out.println(threadNotRun.toString()));
-                System.out.println("所有的子线程都结束了！");  
-                break;  
-            }  
+                shutdownNow.forEach(threadNotRun -> System.out.println(threadNotRun.toString()));
+                System.out.println("所有的子线程都结束了！");
+                break;
+            }
             try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}    
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
 
 //        while(true) {
@@ -94,5 +94,5 @@ public class FixThreadPoolExecutorTest {
 //        	pool.
 //        	
 //        }
-	}
+    }
 }
